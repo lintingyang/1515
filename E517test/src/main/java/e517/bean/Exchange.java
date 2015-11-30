bd83ebc6ef45b012eb80f37311356436d07e663d
lamma <lunacy0909@gmail.com>
2015-11-30 19:57:20 +0800

    first push

---
+++ E517test/src/main/java/e517/bean/Exchange.java
@@ -0,0 +1,81 @@
+package e517.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.Session;
+
+import e517.hibernate.HibernateUtil;
+
+@Entity
+@Table(name="exchange")
+public class Exchange {
+	public Exchange(){
+		
+	}
+	
+public static void main(String[] args) {
+		
+		try {
+			Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+			session.beginTransaction();
+
+//			DeptBean bean = new DeptBean();
+//			bean.setDeptid(60);
+//			bean.setDeptname("TestName");
+//			session.save(bean);
+			
+			Exchange select = (Exchange) session.load(Exchange.class, 1);
+			System.out.println(select);
+			
+			session.getTransaction().commit();
+		} finally {
+			HibernateUtil.closeSessionFactory();
+		}
+
+	}
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int exchangeId;
+	private java.util.Date exgTime;
+	private Integer productAId;
+	private Integer productBId;
+	
+	
+	public int getExchangeId() {
+		return exchangeId;
+	}
+	public void setExchangeId(int exchangeId) {
+		this.exchangeId = exchangeId;
+	}
+	public java.util.Date getExgTime() {
+		return exgTime;
+	}
+	public void setExgTime(java.util.Date exgTime) {
+		this.exgTime = exgTime;
+	}
+	public int getProductAId() {
+		return productAId;
+	}
+	public void setProductAId(Integer productAId) {
+		this.productAId = productAId;
+	}
+	public int getProductBId() {
+		return productBId;
+	}
+	public void setProductBId(Integer productBId) {
+		this.productBId = productBId;
+	}
+	
+	@Override
+	public String toString() {
+		return "[Exchange:" + exchangeId + ", "+ exgTime + ", " + productAId 
+				+ ", " + productBId + "]";
+	}
+
+	
+}
---
+++ E517test/src/main/java/e517/dao/ExchangeDAOHibernate.java
@@ -0,0 +1,92 @@
+package e517.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Session;
+
+import e517.bean.Exchange;
+import e517.bean.ExchangeDAO;
+import e517.hibernate.HibernateUtil;
+
+public class ExchangeDAOHibernate implements ExchangeDAO {
+
+	private Session session;
+	public ExchangeDAOHibernate(Session session) {
+		this.session = session;
+	}
+	public Session getSession() {
+		return session;
+	}
+	
+	public static void main(String[] args){
+		try {
+			Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+			session.beginTransaction();
+			
+			ExchangeDAO dao = new ExchangeDAOHibernate(session);
+//			ExchangeBean bean = dao.select(1);
+//			System.out.println(" ****this is select = "+bean);
+//			
+//			List<ProductBean> list = dao.select();
+//			System.out.println("*****this is select all =" + list);
+			
+			Exchange bean = new Exchange();
+			bean.setExchangeId(1);
+			bean.setExgTime(new java.util.Date());
+			bean.setProductAId(2);
+			bean.setProductBId(6);
+//			int i = dao.insert(bean);
+//			System.out.println("*****this is insert"+ i);
+			
+			int update = dao.update(bean);
+			System.out.println("*****this is insert"+update);
+			
+//			boolean delete = dao.delete(60);
+//			System.out.println("*********this is delete = " + delete);
+			
+			session.getTransaction().commit();
+		} finally {
+			HibernateUtil.closeSessionFactory();
+		}
+		
+	}//end of main
+	
+	public Exchange select(int exchangeId) {
+		return (Exchange) getSession().get(Exchange.class, exchangeId);
+	}
+
+	public List<Exchange> select() {
+		List<Exchange> list = getSession().createQuery("from Exchange").list();
+		return list;
+	}
+
+	public int insert(Exchange bean) {
+		if(bean != null){
+			int pk = (Integer) getSession().save(bean);
+			return pk;
+		}
+		return 0;
+	}
+
+	public int update(Exchange bean) {
+		Exchange sel = (Exchange) getSession().load(Exchange.class, bean.getExchangeId());
+		if (sel != null){
+			sel.setExgTime(bean.getExgTime());
+			sel.setProductAId(bean.getProductAId());
+			sel.setProductBId(bean.getProductBId());
+			return 1;
+		}
+		return 0;
+	}
+
+	public boolean delete(int exchangeId) {
+		Exchange bean = (Exchange) getSession().load(Exchange.class, exchangeId);
+		if(bean != null){
+			getSession().delete(bean);
+			return true;
+		}
+		return false;
+	}
+
+}
