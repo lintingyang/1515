29dbe0cd96c8bc8f211724a7a50a224db4200507
李雙雙 <f228300k@gmail.com>
2015-12-01 15:11:55 +0800

    aaaa

--- src/test/java/tw/com/softleader/e5e5/service/CalculateServiceTest.java
+++ src/test/java/tw/com/softleader/e5e5/service/CalculateServiceTest.java
@@ -22,12 +22,26 @@ public class CalculateServiceTest {
 		cal.mut(2, 3);
 		assertEquals(6, mut);
 	}
+
+	@Test 
+	public void testMin() {
+		CalculateService cal = new CalculateService();
+		int min = cal.min(3, 2);
+		assertEquals(1, min);
+	}
+	
+	@Test
+	public void testDiv(){
+	CalculateService cal = new CalculateService();
+	int div = cal.div(6, 3);
+	assertEquals(2, div);
+	}
+
 	@Test
 	public void tesSub() {
 		CalculateService cal = new CalculateService();
 		int sub = cal.sub(8, 3);
 		assertEquals(5, sub);
 	}
-	
 
 }
