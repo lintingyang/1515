2983cd2ffcc0336518ad37ea8a101cad1e8bf3fc
eliasyao <eliasyaoyj@gmail.com>
2015-12-02 20:00:54 +0800

    加 AdminDao and AdminDaoTest

---
+++ src/test/java/tw/com/softleader/e5e5/dao/AdminDaoTest.java
@@ -0,0 +1,47 @@
+package tw.com.softleader.e5e5.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import tw.com.softleader.e5e5.E5e5App;
+import tw.com.softleader.e5e5.entity.Admin;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes={E5e5App.class})
+public class AdminDaoTest {
+
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	@Autowired
+	private AdminDao adminDao;
+	
+	@Test
+	public void testFindAll() {
+	
+		Admin admin = new Admin();
+		admin.setPassword("123456");;
+		admin.setName("xyz");
+		
+		adminDao.save(admin);
+		
+		Admin dbEntity = adminDao.findOne(1);
+		log.debug(dbEntity);
+		
+//		List<Admin> boardCategories = adminDao.findByName("xyz");
+		
+//		for(Admin b:boardCategories) {
+//			log.debug(b);
+//		}
+		
+	}
+
+}
