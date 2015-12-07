9d4c41d21d6328d12009257d45b08b04e2121034
李雙雙 <f228300k@gmail.com>
2015-12-07 17:35:47 +0800

    add advertisement

---
+++ src/test/java/tw/com/softleader/e5e5/dao/AdvertisementDaoTest.java
@@ -0,0 +1,19 @@
+package tw.com.softleader.e5e5.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import tw.com.softleader.e5e5.E5e5App;
+
+public class AdvertisementDaoTest {
+
+	@Test
+	public void testFindAll() {
+		
+	}
+
+}
