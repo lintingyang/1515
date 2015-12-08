6aa52f763943025983d013212516bd437ee35fa4
李雙雙 <f228300k@gmail.com>
2015-12-08 14:20:59 +0800

    add tag

--- src/test/java/tw/com/softleader/e5e5/dao/TagDaoTest.java
+++ src/test/java/tw/com/softleader/e5e5/dao/TagDaoTest.java
@@ -1,7 +1,5 @@
 package tw.com.softleader.e5e5.dao;
 
-import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,23 +8,22 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import tw.com.softleader.e5e5.E5e5App;
-import tw.com.softleader.e5e5.entity.Advertisement;
-
+import tw.com.softleader.e5e5.entity.Tag;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes={E5e5App.class})
-public class AdvertisementDaoTest {
-	private Logger log = Logger.getLogger(this.getClass());
+public class TagDaoTest {
+	private Logger log =Logger.getLogger(this.getClass());
 	
 	@Autowired
-	private AdvertisementDao advertisementDao;
+	private TagDao tagDao;
 	
 	@Test
-	public void testFindAll() {
-		List<Advertisement> list = advertisementDao.findAll();
-		for( Advertisement advertisement:list){
-			log.error(advertisement);
-		}
+	public void testCrud() {
+		Tag tag = new Tag();
+		tag.setName("hahaha");
+		tagDao.save(tag);
+		tagDao.delete(1);
 	}
 
 }
