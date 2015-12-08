22ce220ea3a6bc32e96aa46ada54376c668ae9b5
Student <Student@III-ITC>
2015-12-08 14:50:11 +0800

    ya update service.java!

--- src/test/java/tw/com/softleader/e5e5/service/ChatServiceTest.java
+++ src/test/java/tw/com/softleader/e5e5/service/ChatServiceTest.java
@@ -10,31 +10,33 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import tw.com.softleader.e5e5.E5e5App;
+import tw.com.softleader.e5e5.dao.ChatDao;
+import tw.com.softleader.e5e5.dao.UserDao;
 import tw.com.softleader.e5e5.entity.Chat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes={E5e5App.class})
 public class ChatServiceTest {
 
-private Logger log = Logger.getLogger(this.getClass());
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	@Autowired
+	private ChatDao chatDao;
+	@Autowired
+	private UserDao userDao;
+	@Test
+	public void testGetLastThreeChats() {
+		ChatService cs = new ChatService(chatDao,userDao);
+		List<Chat> chats=cs.getLastThreeChats();
+		for(Chat c: chats){
+			log.debug("Test: "+c);
+		}
+	}
+	@Test
+	public void testPostChat() {
+		ChatService cs = new ChatService(chatDao,userDao);
+		cs.postChat(1, "測試");
+	}
 
-//	@Autowired
-//	private ChatService chatService;
-//	@Test
-//	public void testGetLastThreeChats() {
-//		ChatService chatService = new ChatService();
-//		List<Chat> chats=chatService.getLastThreeChats();
-//		if (chats!=null) {
-//			for (Chat c : chats) {
-//				log.debug(c);
-//			} 
-//		}
-//	}
-//
-//	@Test
-//	public void testPostChat() {
-//		ChatService chatService = new ChatService();
-//		chatService.postChat(1, "測試");
-//	}
 
 }
