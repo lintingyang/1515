a2d8237ec584316c6b0d7341746f3cd45679268c
Student <Student@III-ITC>
2015-12-08 14:33:24 +0800

    Add working chatservices sample

--- src/test/java/tw/com/softleader/e5e5/dao/ChatDaoTest.java
+++ src/test/java/tw/com/softleader/e5e5/dao/ChatDaoTest.java
@@ -1,9 +1,7 @@
 package tw.com.softleader.e5e5.dao;
 
 
-import static org.junit.Assert.*;
-
-import java.util.Date;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -14,7 +12,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import tw.com.softleader.e5e5.E5e5App;
 import tw.com.softleader.e5e5.entity.Chat;
-import tw.com.softleader.e5e5.entity.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes={E5e5App.class})
@@ -29,15 +26,23 @@ public class ChatDaoTest {
 	
 	@Test
 	public void testSaveS() {
-		Chat chat = new Chat();
-		User userone = userDao.findOne(4);
-		log.debug(userone);
+//		Chat chat = new Chat();
+//		User userone = userDao.findOne(4);
+//		log.debug(userone);
+//		
+//
+//		chat.setMessages("有問題想問一下");
+//		chat.setSendTime(new Date());
+//		chat.setUser(userone);
+//		log.debug(chatDao.save(chat));
 		
+		List<Chat> chats = chatDao.findAllOrderBySendTime();
+		for(Chat c: chats){
+			log.debug("Test: "+c);
+		}
 
-		chat.setMessages("有人在嘛");
-		chat.setSendTime(new Date());
-		chat.setUser(userone);
-		log.debug(chatDao.save(chat));
+			
+				
 	}
 
 }
