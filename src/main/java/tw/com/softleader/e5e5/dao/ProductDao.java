0c12c77a323ef70ebecc543d04cdb28b75cdd924
justinymc <justin.y.m.c@gmail.com>
2015-12-09 17:16:17 +0800

    重整 entity/product.java 新增 找尋點擊率最高的產品 新增 找尋上傳最多產品的會員

--- src/main/java/tw/com/softleader/e5e5/dao/ProductDao.java
+++ src/main/java/tw/com/softleader/e5e5/dao/ProductDao.java
@@ -3,9 +3,18 @@ package tw.com.softleader.e5e5.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import tw.com.softleader.e5e5.entity.Product;
 
 public interface ProductDao extends JpaRepository<Product, Integer> {
 	public List<Product> findByName(String name);
+	
+	//find most clicked product
+	@Query(value="SELECT TOP 1 name FROM product ORDER BY click_times DESC", nativeQuery=true)
+	public String findByTopClickedProduct();
+	
+	//find user who posted products most
+	@Query(value="SELECT TOP 1 user_id FROM product GROUP BY user_id ORDER BY COUNT (*) DESC", nativeQuery=true)
+	public int findByUserCounts();
 }
