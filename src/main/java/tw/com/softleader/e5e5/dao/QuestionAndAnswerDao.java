f22024c7bb62822e8f968043d74e738bc7c01086
lamma <lunacy0909@gmail.com>
2015-12-14 21:05:57 +0800

    some edit

--- src/main/java/tw/com/softleader/e5e5/dao/QuestionAndAnswerDao.java
+++ src/main/java/tw/com/softleader/e5e5/dao/QuestionAndAnswerDao.java
@@ -3,12 +3,14 @@ package tw.com.softleader.e5e5.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import tw.com.softleader.e5e5.entity.Product;
 import tw.com.softleader.e5e5.entity.QuestionAndAnswer;
 
 public interface QuestionAndAnswerDao extends JpaRepository<QuestionAndAnswer, Integer> {
 	
-	public List<QuestionAndAnswer> findByProduct (Product product);
+	@Query(value="select question, answer FROM question_and_answer qa WHERE product_id = ?0", nativeQuery=true)
+	public List<Object[]> findByProduct (Product product);
 	
 }
