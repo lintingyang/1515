54f0d9679ef72e3931b9cf94abc879121b0ba015
Peter Chen <pc2720@hotmail.com>
2015-12-16 17:52:44 +0800

    add chat servlet

--- src/main/java/tw/com/softleader/e5e5/controller/ChatController.java
+++ src/main/java/tw/com/softleader/e5e5/controller/ChatController.java
@@ -1,41 +1,71 @@
 package tw.com.softleader.e5e5.controller;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import tw.com.softleader.e5e5.entity.Chat;
+import tw.com.softleader.e5e5.entity.User;
+import tw.com.softleader.e5e5.service.ChatService;
 
 @Controller
 @RequestMapping(value = "/chats")
 public class ChatController {
-	
-	@RequestMapping(value = "/list") // ** /chat/list
+	private Logger log = Logger.getLogger(this.getClass());
+	@Autowired
+	private ChatService chatService;
+
+	@RequestMapping(value = "/list") // ** /chats/list
 	public String list(Model model) {
+		List<Chat> chats = chatService.getLastThreeChats();
+		model.addAttribute("beans", chats);
 		return "/chat/list";
 	}
-	@RequestMapping(value = "/query") 
-	public String query(Model model) {
-		return "/chat/list.jsp";
+
+	@RequestMapping(value = "/query")
+	public String query(Model model, @ModelAttribute("user") User user) {
+		chatService.postChat(user.getId(), "messages_no");
+		List<Chat> chats = chatService.getLastThreeChats();
+		model.addAttribute("beans", chats);
+		return "/chat/list";
 	}
+//	@RequestMapping(value = "/query")
+//	public String query(Model model, @RequestParam("id") Integer id) {
+//		chatService.postChat(id, "messages");
+//		List<Chat> chats = chatService.getLastThreeChats();
+//		model.addAttribute("beans", chats);
+//		return "/chat/list";
+//	}
+
 	@RequestMapping(value = "/delete")
 	public String delete(Model model) {
-		return "/chat/list.jsp";
+		return "/chat/list";
 	}
+
 	@RequestMapping(value = "/add")
 	public String add(Model model) {
-		return "/chat/add.jsp";
+		return "/chat/add";
 	}
+
 	@RequestMapping(value = "/insert")
 	public String insert(Model model) {
-		return "/chat/add.jsp";
+		return "/chat/add";
 	}
+
 	@RequestMapping(value = "/edit")
 	public String edit(Model model) {
-		return "/chat/edit.jsp";
+		return "/chat/edit";
 	}
+
 	@RequestMapping(value = "/update")
 	public String update(Model model) {
-		return "/chat/add.jsp";
+		return "/chat/add";
 	}
-	
 
 }
--- src/main/java/tw/com/softleader/e5e5/dao/ChatDao.java
+++ src/main/java/tw/com/softleader/e5e5/dao/ChatDao.java
@@ -11,7 +11,8 @@ import tw.com.softleader.e5e5.entity.Chat;
 public interface ChatDao extends JpaRepository<Chat, Integer> {
 
 	//find last 3 chat message by post time
-	@Query(value ="select top 3 * from Chat c where send_time is not null order by send_time desc", nativeQuery = true)
+//	@Query(value ="select top 3 * from Chat c where send_time is not null order by send_time desc", nativeQuery = true)
+	@Query(value ="select * from Chat c where send_time is not null order by send_time desc", nativeQuery = true)
 	public List<Chat> findAllOrderBySendTime();
 
 }
