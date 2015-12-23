3ccc2290c24717942c3e8a4a0ba13b0e7bb42b20
shuang <f228300k@gmail.com>
2015-12-23 18:13:02 +0800

    commit

--- src/main/java/tw/com/softleader/e5e5/controller/AdminController.java
+++ src/main/java/tw/com/softleader/e5e5/controller/AdminController.java
@@ -1,23 +1,17 @@
 package tw.com.softleader.e5e5.controller;
 
-
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.imageio.ImageIO;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import tw.com.softleader.e5e5.entity.Admin;
 import tw.com.softleader.e5e5.service.AdminService;
@@ -37,37 +31,21 @@ public class AdminController {
 		return "/admin/list";
 	}
 
-	@RequestMapping(value = "/query") // 用帳號搜尋管理員資料
-	public String query(Model model, @RequestParam("account") String account) {
+	@ResponseBody
+	@RequestMapping(value = "/query" ) // 用帳號搜尋管理員資料
+	public List<Admin> query(@RequestBody Admin account) {
+		log.error("account ========"+account);
+		Admin admin1 = adminService.findByAccount(account.getAccount());
+		List<Admin> list = new ArrayList<Admin>();
+		list.add(admin1);
+		
 
-		account = account.replaceAll("\\s+", "");
-		if (account != null && !account.equals("")) {  
-			Admin admin1 = adminService.findByAccount(account);
-			if (admin1 != null) {  //判斷是否有找到此帳號
-				List<Admin> list = new ArrayList<Admin>();
-				list.add(admin1);
-				model.addAttribute("entity", list);
-			}else{
-				model.addAttribute("error","查無此帳號");
-			}
-		}else{
-			model.addAttribute("error", "請輸入查詢資料");
-			List<Admin> list = adminService.findAllAdmins();
-			model.addAttribute("entity", list);
-		}
-		return "/admin/list";
+		return list;
 
 	}
-	// @RequestMapping(value = "/query")
-	// public String query(Model model,@ModelAttribute@ String admin){
-	// Admin admin1 = adminService.findByAccount(admin);
-	// model.addAttribute("aaa",admin1);
-	// return "/admin/list";
-	// }
-	//
 
 	@RequestMapping(value = "/delete")
-	public String delete(Model model,@ModelAttribute Admin admin) {
+	public String delete(Model model, @ModelAttribute Admin admin) {
 		adminService.deleteById(admin.getId());
 		List<Admin> list = adminService.findAllAdmins();
 		model.addAttribute("entity", list);
@@ -101,35 +79,35 @@ public class AdminController {
 	}
 
 	@RequestMapping(value = "/update")
-	public String update(Model model,@RequestParam("name")String name,@RequestParam("account")String account,
-			@RequestParam("password")String password,@RequestParam("authority")String authority,
-			@RequestParam("email")String email){
+	public String update(Model model, @RequestParam("name") String name, @RequestParam("account") String account,
+			@RequestParam("password") String password, @RequestParam("authority") String authority,
+			@RequestParam("email") String email) {
 		int result = adminService.updateAdmin(account, authority, name, password, email);
-		if(result == 1){
+		if (result == 1) {
 			model.addAttribute("error", "修改成功！");
-		}else if(result == 0){
-			model.addAttribute("error","修改失敗！");
+		} else if (result == 0) {
+			model.addAttribute("error", "修改失敗！");
 		}
 		List<Admin> list = adminService.findAllAdmins();
 		model.addAttribute("entity", list);
-		
+
 		return "/admin/list";
 	}
-	
-//	@RequestMapping(value = "/upload")
-//	public String insert(Model model,@RequestParam("file") MultipartFile file) {
-//		BufferedImage src = null;
-//		if (!file.isEmpty()) {
-//			try {
-//				src = ImageIO.read(new ByteArrayInputStream(file.getBytes()));
-//				 File destination = new File("src/main/webapp/WEB-INF/images/03.jpg"); 
-//				 ImageIO.write(src, "jpg", destination);
-//			} catch (IOException e) {
-//				log.error("exception");
-//			}
-//		}
-//		return "/admin/list";
-//	}
 
+	// @RequestMapping(value = "/upload")
+	// public String insert(Model model,@RequestParam("file") MultipartFile
+	// file) {
+	// BufferedImage src = null;
+	// if (!file.isEmpty()) {
+	// try {
+	// src = ImageIO.read(new ByteArrayInputStream(file.getBytes()));
+	// File destination = new File("src/main/webapp/WEB-INF/images/03.jpg");
+	// ImageIO.write(src, "jpg", destination);
+	// } catch (IOException e) {
+	// log.error("exception");
+	// }
+	// }
+	// return "/admin/list";
+	// }
 
 }
