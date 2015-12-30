e222034479319d03a23adaa844a2179c50bd9f39
Gary Lee <jakarta99@gmail.com>
2015-12-30 18:16:59 +0800

    AdminTest sample

--- src/test/java/tw/com/softleader/e5e5/dao/AdminDaoTest.java
+++ src/test/java/tw/com/softleader/e5e5/dao/AdminDaoTest.java
@@ -1,41 +1,58 @@
 package tw.com.softleader.e5e5.dao;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import lombok.extern.log4j.Log4j;
 import tw.com.softleader.e5e5.common.AbstractTest;
 import tw.com.softleader.e5e5.entity.Admin;
 
-
+@Log4j
 public class AdminDaoTest extends AbstractTest {
 
-	private Logger log = Logger.getLogger(this.getClass());
+	//private Logger log = Logger.getLogger(this.getClass());
 	
 	@Autowired
 	private AdminDao adminDao;
 	
 
 	@Test
-	public void testFindAll() {
+	public void testCrud() {
 	
+		// 1. select 
+		List<Admin> entities = adminDao.findAll();
 		
-		Admin dbEntity = adminDao.findByAccount("1");
-		log.debug(dbEntity);
-//		Admin admin = new Admin();
-//		admin.setPassword("123456");
-//		admin.setName("xyz");		
-//		adminDao.save(admin);
+		assertEquals(0, entities.size());
+		
+		Admin newAdmin = new Admin();
+		
+		newAdmin.setName("admin");
+		
+		newAdmin = adminDao.save(newAdmin);
+		assertEquals(1, newAdmin.getId());
+		
+		entities = adminDao.findAll();
+		assertEquals(1, entities.size());
+		
+		
+		Admin dbAdmin = adminDao.findOne(newAdmin.getId());
+		dbAdmin.setEmail("test@test.com");
+		dbAdmin = adminDao.save(dbAdmin);
+		
+		Admin testAdmin = adminDao.findOne(newAdmin.getId());
+		assertEquals("test@test.com", testAdmin.getEmail());
+		
+		adminDao.delete(newAdmin.getId());
+		
+		entities = adminDao.findAll();
 
 		
-		List<Admin> boardCategories = adminDao.findByName("abc");
-		
-		for(Admin b:boardCategories) {
-			log.debug(b);
-		}
-		
+		assertEquals(0, entities.size());
 	}
 
 }
