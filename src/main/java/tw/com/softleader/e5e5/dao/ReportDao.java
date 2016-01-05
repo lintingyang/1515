0549eca8f5b3f9012c46eef85d842a44b588ae70
justinymc <justin.y.m.c@gmail.com>
2016-01-05 13:36:55 +0800

    fix report

--- src/main/java/tw/com/softleader/e5e5/dao/ReportDao.java
+++ src/main/java/tw/com/softleader/e5e5/dao/ReportDao.java
@@ -12,6 +12,10 @@ public interface ReportDao extends JpaRepository<Report, Integer> {
 	@Query(value="SELECT * FROM report WHERE is_passed is null ORDER BY report_time DESC", nativeQuery = true)
 	public List<Report> findAllUnread();
 	
+	//尚未處理之物品
+	@Query(value="SELECT * FROM report WHERE is_passed is null and product_id is not null ORDER BY report_time", nativeQuery = true)
+	public List<Report> findAllProductUnread();
+
 	public List<Report> findAllByOrderByReportTimeDesc();
 	
 }
