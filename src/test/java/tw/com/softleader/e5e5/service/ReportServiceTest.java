539b9ae7d4e3cf2885217772d13781b4598a0197
justinymc <justin.y.m.c@gmail.com>
2016-01-05 11:19:45 +0800

    改report entity

--- src/test/java/tw/com/softleader/e5e5/service/ReportServiceTest.java
+++ src/test/java/tw/com/softleader/e5e5/service/ReportServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import tw.com.softleader.e5e5.E5e5App;
 import tw.com.softleader.e5e5.dao.ExchangeDao;
 import tw.com.softleader.e5e5.dao.ProductDao;
+import tw.com.softleader.e5e5.entity.enums.TrueFalse;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes={E5e5App.class})
@@ -23,7 +24,7 @@ public class ReportServiceTest {
 	@Test
 	public void testUpdateIsPassed() {
 		
-		int result = reportService.updateIsPassed(5, 'F');
+		int result = reportService.updateIsPassed(5, TrueFalse.FALSE);
 		log.info("****審核結果==" + result);
 	}
 	
