d31f131f774d62a4932f9a006d61f634ac339f29
Lin <slaverbread@gmail.com>
2016-01-06 17:29:32 +0800

    all

--- src/main/java/tw/com/softleader/e5e5/service/FocusUserListService.java
+++ src/main/java/tw/com/softleader/e5e5/service/FocusUserListService.java
@@ -6,25 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import tw.com.softleader.e5e5.common.dao.OurDao;
+import tw.com.softleader.e5e5.common.model.Message;
+import tw.com.softleader.e5e5.common.service.OurService;
 import tw.com.softleader.e5e5.dao.FocusUserListDao;
 import tw.com.softleader.e5e5.dao.UserDao;
 import tw.com.softleader.e5e5.entity.FocusUserList;
 import tw.com.softleader.e5e5.entity.User;
+import tw.com.softleader.e5e5.entity.UserBanList;
 
 @Service
-public class FocusUserListService {
+public class FocusUserListService  extends OurService<FocusUserList>{
 	
 	@Autowired
-	private FocusUserListDao fulDao;
+	private FocusUserListDao focusUserListDao;
 	@Autowired
-	private UserDao uDao;
+	private UserDao userDao;
 	
 
 	
 	@Transactional
 	public List<FocusUserList> findOneUser(int id){
-		if(uDao.findById(id)!=null){
-		List<FocusUserList> result = fulDao.findByOneUser(id);
+		if(userDao.findById(id)!=null){
+		List<FocusUserList> result = focusUserListDao.findByOneUser(id);
 		return result;
 		}
 		return null;
@@ -32,19 +36,19 @@ public class FocusUserListService {
 	
 	@Transactional
 	public int insert (int userAId , int userBId){
-		List<FocusUserList> fuls = fulDao.findByOneUser(userAId);
+		List<FocusUserList> fuls = focusUserListDao.findByOneUser(userAId);
 		boolean temp = false;
 		for(FocusUserList ful : fuls){
-			if((int)ful.getUserByUserBId().getId()!= userBId ){
+			if((int)ful.getUserBId().getId()!= userBId ){
 				temp=true;
 			}
 		}
 		
 		if(!temp){
 			FocusUserList fulNew = new FocusUserList();
-			fulNew.setUserByUserAId(new User(userAId));
-			fulNew.setUserByUserBId(new User(userBId));
-			fulDao.save(fulNew);
+			fulNew.setUserAId(new User(userAId));
+			fulNew.setUserBId(new User(userBId));
+			focusUserListDao.save(fulNew);
 			return 1;	
 				}
 			return 0;
@@ -52,12 +56,35 @@ public class FocusUserListService {
 			
 	@Transactional
 	public boolean deletOne(int userAId , int userBId){
-		FocusUserList ful2 = fulDao.findByOnly(userAId, userBId);
+		FocusUserList ful2 = focusUserListDao.findByOnly(userAId, userBId);
 		if(ful2!=null){
-			fulDao.delete(ful2.getId());
+			focusUserListDao.delete(ful2.getId());
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public OurDao<FocusUserList> getDao() {
+		return focusUserListDao;
+	}
+
+	@Override
+	protected List<Message> validateInsert(FocusUserList entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected List<Message> validateUpdate(FocusUserList entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected List<Message> validateDelete(int id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 		
 	
