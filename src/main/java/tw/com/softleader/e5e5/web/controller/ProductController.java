618aec73a2b44c1f3c4fee879adcfcf22159258a
yuming <chang-yu-ming@hotmail.com>
2016-01-08 02:12:00 +0800

    新增產品（圖片ＯＫ） 差時間控制 彈出視窗

--- src/main/java/tw/com/softleader/e5e5/web/controller/ProductController.java
+++ src/main/java/tw/com/softleader/e5e5/web/controller/ProductController.java
@@ -1,11 +1,11 @@
 package tw.com.softleader.e5e5.web.controller;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,16 +18,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import tw.com.softleader.e5e5.service.ExchangeService;
 import tw.com.softleader.e5e5.service.ProductPictureService;
 import tw.com.softleader.e5e5.entity.Exchange;
 import tw.com.softleader.e5e5.entity.Product;
-import tw.com.softleader.e5e5.entity.ProductCategory;
 import tw.com.softleader.e5e5.entity.ProductPicture;
 import tw.com.softleader.e5e5.entity.User;
-import tw.com.softleader.e5e5.entity.enums.TrueFalse;
-import tw.com.softleader.e5e5.security.entity.SecRole;
 import tw.com.softleader.e5e5.service.ProductService;
 
 @Controller
@@ -41,6 +39,9 @@ public class ProductController {
 	public ProductPictureService productPictureService;
 	@Autowired
 	public ExchangeService exchangeService;
+	
+	@Autowired
+	private ServletContext servletContext;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public String editPage(@PathVariable("id") final int id, final Model model) {
@@ -63,16 +64,16 @@ public class ProductController {
 		return exchanges;
 	}
 	
-	//銘 新增商品
+	//新增商品
 	@RequestMapping(value = "/add")
 	public String add(Model model) {
 		return "/e715/product/proAdd";
 	}
-	
-	
+	//新增商品
 	@RequestMapping(value = "/insert")
 	public String insert(Model model, @ModelAttribute Product product, 
-									  @RequestParam("pCategory") int pCategory, 
+									  @RequestParam("pCategory") int pCategory,
+									  @RequestParam("pPicture") MultipartFile pPicture,
 									  @RequestParam("pStatusBad") String pStatusBad, 
 									  @RequestParam("pWishItem") String pWishItem, 
 									  @RequestParam("pyyyy") String pyyyy, 
@@ -82,10 +83,9 @@ public class ProductController {
 									  @RequestParam("pmm") String pmm,
 									  HttpSession session) {
 		
-		User userData = (User) session.getAttribute("user");
-		System.out.println("product========================" + product);
-		Map<String, String> errorMessage = new HashMap<>();
-		session.setAttribute("errorMsg", errorMessage);
+		//錯誤訊息顯示
+//		Map<String, String> errorMessage = new HashMap<>();
+//		session.setAttribute("errorMsg", errorMessage);
 //		if(product.getName() == null || product.getName().trim().length() == 0){
 //			errorMessage.put("name", "請輸入商品標題/名稱");
 //		}
@@ -103,45 +103,53 @@ public class ProductController {
 //		if(errorMessage != null && !errorMessage.isEmpty()) {
 //			return "redirect:/product/insert";
 //		}
-//		if (!month.equals("") && month.length() < 2) {
-//			month = "0" + month;
-//		}
-//		if (!day.equals("") && day.length() < 2) {
-//			day = "0" + day;
-//		}
-//		String str = year + "-" + month + "-" + day+" 00:00"; //"1986-04-08";
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		
+		//取userId
+		User userData = (User) session.getAttribute("user");
+		
+		//時間處理
 		LocalDateTime dateTime =null;
-		try {
-			if(pyyyy == null){
-				dateTime = null;				
-			}else{
-				int year = Integer.parseInt(pyyyy);
-				int month = Integer.parseInt(pMM);
-				int day = Integer.parseInt(pdd);
-				int hour = Integer.parseInt(pHH);
-				int minute = Integer.parseInt(pmm);
-				dateTime = LocalDateTime.of(year, month, day, hour, minute);
-			}
-			
-		} catch (Exception e) {
+		if(pyyyy == null){
+			dateTime = null;				
+		}else{
+			int year = Integer.parseInt(pyyyy);
+			int month = Integer.parseInt(pMM);
+			int day = Integer.parseInt(pdd);
+			int hour = Integer.parseInt(pHH);
+			int minute = Integer.parseInt(pmm);
+			dateTime = LocalDateTime.of(year, month, day, hour, minute);
 		}
-		int number = productService.insert(product.getName(),
+		
+		//存入資料
+		Product newProduct = productService.insert(product.getName(),
 				userData.getId(), 
 				pCategory, 
 				product.getStatus() +"(" +pStatusBad +")", 
 				product.getDescription(), 
-				dateTime, 
+				null, 
 				product.getTransactionTime(), 
 				product.getLocation(), 
 				product.getTradeWay(), product.getWishItem() +"(" +pWishItem +")", 
 				product.getPostStatus());
-		System.out.println("number========================" + number);
-		if(number == 1){
+		System.out.println("newProduct========================" + newProduct);
+		if(newProduct != null){
 			model.addAttribute("result", "新增成功");
+			session.setAttribute("new", newProduct);
 		}else{
 			model.addAttribute("result", "新增失敗");
 		}
+//		//get ProductId
+//		Product p = productService. 
+		
+		//存取productPicture
+		String path = productPictureService.upLoadImage(newProduct.getId(), servletContext, pPicture);
+		int numPicture = productPictureService.insertImage(newProduct.getId(), path);
+		if(numPicture == 1){
+			model.addAttribute("picResult", "圖片新增成功");
+		}else{
+			model.addAttribute("picResult", "圖片新增失敗");
+		}
+		
 		return "/e715/product/proAdd";
 		
 	}
