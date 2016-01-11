d2774661ea354a87089cbfce533228f60c84ffdd
eliasyao <eliasyaoyj@gmail.com>
2016-01-11 21:25:12 +0800

    圖片待完成

--- src/main/java/tw/com/softleader/e5e5/service/ProductService.java
+++ src/main/java/tw/com/softleader/e5e5/service/ProductService.java
@@ -53,39 +53,22 @@ public class ProductService extends OurService<Product> {
 		} else if (orderby.equals("誠信")){
 			
 		}
-		// if (orderby.equals("熱門")) {
-		//
-		// if (namelike == null || namelike.equals("")) {
-		// list = productService.getProductsOrderByClickTimes();
-		// } else {
-		// list = productService.findeOrderByClickTime(namelike,categoryname);
-		// }
-		// } else if (orderby.equals("最新")) {
-		//
-		// if (namelike == null || namelike.equals("")) {
-		// list = productService.getProductsOrderByPostTime();
-		// } else {
-		// list =
-		// productService.findByProductOrderByPostTime(namelike,categoryname);
-		// }
-		//
-		// } else if (orderby.equals("誠信")) {
-		//
-		// } else if (orderby.equals("推薦")) {
-		//
-		// }
-		// return list;
 
 		return list;
 	}
-
-	@Transactional
+	@Transactional//shuang
+	public List<Product> findByNameAndStatus(int id){
+		List<Product> list = productDao.findByUserIdAndStatue(id);
+		
+		return list;
+	}
+	@Transactional//shuang
 	public List<Product> findeOrderByClickTime(String productName, String categoryName) {
 		List<Product> list = productDao.findByProdcutOrderByClickTimes(productName, categoryName);
 		return list;
 	}
 
-	@Transactional
+	@Transactional//shuang
 	public List<Product> findByProductOrderByPostTime(String productName, String categoryName) {
 		List<Product> list = productDao.findByProductOrderByPostTime(productName, categoryName);
 		return list;
@@ -256,12 +239,11 @@ public class ProductService extends OurService<Product> {
 	}
 	// (14)編輯產品
 	@Transactional
-	public Product update(String name, int user, int category, String status, String description,
+	public Product update(int id ,String name, int category, String status, String description,
 			LocalDateTime deadline, LocalDateTime startTime, Time transactionTime, String location, String tradeWay,
 			String wishItem, TrueFalse postStatus) {
-		Product product = new Product();
+		Product product = productDao.findOne(id);
 		product.setName(name);
-		product.setUserId(userDao.findOne(user));
 		product.setProductCategory(productCategoryDao.findOne(category));
 		product.setStatus(status);
 		product.setDescription(description);
