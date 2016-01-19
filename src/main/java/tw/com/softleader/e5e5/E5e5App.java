45975b7a759052dba8f6a4706e7b00db5ef7ddd1
Lin <slaverbread@gmail.com>
2016-01-19 17:24:47 +0800

    111

--- src/main/java/tw/com/softleader/e5e5/E5e5App.java
+++ src/main/java/tw/com/softleader/e5e5/E5e5App.java
@@ -3,13 +3,22 @@ package tw.com.softleader.e5e5;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 import tw.com.softleader.e5e5.service.CalculateService;
 
 @SpringBootApplication
-public class E5e5App {
+public class E5e5App extends SpringBootServletInitializer{
 	
 	private static Logger log = Logger.getLogger(E5e5App.class);
+	
+	  @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(E5e5App.class);
+	    }
+
+	
 	
 	public static void main(final String[] args) {
 		SpringApplication.run(E5e5App.class, args);
